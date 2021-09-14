6493316bddb19ee4e262eba573625a17a29bed92
Junior <Junior@host.docker.internal>
2021-09-14 09:44:32 -0300

    Versão Inicial

---
+++ G212/src/main/java/fatec/scc/G212/G2122Application.java
@@ -0,0 +1,13 @@
+package fatec.scc.G212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2122Application.class, args);
+	}
+
+}
---
+++ G212/src/test/java/fatec/scc/G212/G2122ApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.scc.G212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2122ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
